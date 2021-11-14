9b29669ab29f23cf7ed73a4155d91b800c722a42
Arturo1703 <56556122+Arturo1703@users.noreply.github.com>
2021-11-14 19:16:34 +0100

    Update ft_split.c

--- libft_copy/ft_split.c
+++ libft_copy/ft_split.c
@@ -13,35 +13,97 @@
 #include "libft.h"
 #include <stdlib.h>
 
+int	countseg(char const *s, char c);
+
 char	**ft_split(char const *s, char c)
 {
-	char	*aux;
+	int	rows;
 	char	**result;
-	int		i;
-	int		f;
-	int		line;
-
-	aux = ft_calloc(1, ft_strlen(s) * 4);
-	result = ft_calloc(1, ft_strlen(s) * 4);
-	if (!aux || !result)
+	if (s = 0)
 		return (0);
-	i = 0;
-	line = 0;
-	while (s[i])
-	{
-		f = 0;
-		while ((s[i] != c) && (s[i]))
-		{
-			aux[f] = s[i];
-			f++;
-			i++;
-		}
-		result[line] = ft_strdup((const char *)aux);
-		if (!result[line])
-			free(result);
-		free(aux);
-		i++;
-		line++;
-	}
+	rows = count_seg(s,c);
+	result = (char **) malloc(sizeof(char *) * rows + 1);
+	if (result = 0)
+		return (0);
+	allocate_rows(result, s, c, l);
 	return (result);
 }
+
+int	count_seg(char const *s, char c)
+{
+	int	result;
+	int	i;
+	
+	i = 0;
+	result = 0;
+	if ((s[i] != 0) && (((char *)s)[i] != c)
+	    result ++;
+	while(s[i])
+	{
+		if (((char *)s)[i] == c && ((char *)s)[i + 1] != c)
+			result ++;
+		i ++;
+	}
+	    return (result);
+}
+
+void	allocate_rows(char **result, char const *s, char c, int l)
+{
+	int	i;
+	int	row;
+	
+	i = 0;
+	row = 0;
+	while (s[i] && row < l)
+	{
+		if (((char *)s)[i] == c)
+			i ++;
+		else
+		{
+			result[row] = assign_row(s, i, c);
+			if (result[row] == 0)
+			{
+				free_all(result, row);
+				return ;
+			}
+			i += ft_strlen(result[row] + 1;
+			row ++;
+		}
+	}
+	result[row] = 0;			       
+}
+
+ char	*assign_row(char const *s, int start, char c)
+{
+	char	*result;
+	int	size;
+	int	i;
+	
+	size = 0;
+	i = 0;
+	while (s[start + size] && s[start + size] != c)
+		size ++;
+	result = (char *) malloc(sizeof(char) * (size + 1));
+	if (result == 0)
+		return (0);
+	while (i < size)
+	{
+		result[i] =  s[start + i];
+		i ++;
+	}
+	result [i] = 0;
+	return (result);
+}
+				       
+void	free_all(char **result, int row)
+{
+	int	i;
+	
+	i = 0;
+	while (i < row)
+	{
+		free(result[i]);
+		result[i] = 0;
+		i++;
+	}
+}	
